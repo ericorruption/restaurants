cb096571f516b75ce95c43d09420d8f38cfd2562
Eric Quanz <eric.quanz@gmail.com>
2021-09-25 22:00:55 +0200

    Scaffold AuthContext in client

--- src/3.infrastructure/user-interface/App.tsx
+++ src/3.infrastructure/user-interface/App.tsx
@@ -1,19 +1,28 @@
 import { lazy, Suspense } from "react";
 import type { FunctionComponent } from "react";
 
-import { useUser } from "./auth";
-
 import "./App.css";
+import { AuthProvider, useAuth } from "./AuthContext";
 
 const AuthenticatedApp = lazy(() => import("./AuthenticatedApp"));
 const UnauthenticatedApp = lazy(() => import("./UnauthenticatedApp"));
 
-export const App: FunctionComponent = () => {
-  const user = useUser();
+export const App: FunctionComponent = () => (
+  <AppProviders>
+    <AppGate />
+  </AppProviders>
+);
+
+const AppGate: FunctionComponent = () => {
+  const { isLoggedIn } = useAuth();
 
   return (
     <Suspense fallback={<>Loading...</>}>
-      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      {isLoggedIn ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </Suspense>
   );
 };
+
+const AppProviders: FunctionComponent = ({ children }) => (
+  <AuthProvider>{children}</AuthProvider>
+);
---
+++ src/3.infrastructure/user-interface/AuthContext.tsx
@@ -0,0 +1,62 @@
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  FunctionComponent,
+  useEffect,
+  useState,
+  useContext,
+} from "react";
+
+export type Token = string | undefined;
+
+interface IAuthContext {
+  token: Token;
+  setToken: Dispatch<SetStateAction<Token>>;
+  isLoggedIn: boolean;
+  logout: VoidFunction;
+}
+
+const AuthContext = createContext<IAuthContext>({
+  token: undefined,
+  setToken: () => undefined,
+  isLoggedIn: false,
+  logout: () => {},
+});
+
+// export const AUTH_TOKEN = "AUTH_TOKEN";
+
+// https://hasura.io/blog/best-practices-of-using-jwt-with-graphql/#jwt_structure
+export const AuthProvider: FunctionComponent = (props) => {
+  const [token, setToken] = useState<Token>(); // TODO Cookies.get(AUTH_TOKEN)
+
+  useEffect(() => {
+    // if (token) {
+    //   Cookies.set(AUTH_TOKEN, token);
+    // } else {
+    //   Cookies.remove(AUTH_TOKEN);
+    // }
+  }, [token]);
+
+  // if (weAreStillWaitingToGetTheUserData) {
+  //   return <FullPageSpinner />
+  // }
+
+  // TODO move mutations here? e.g.:
+  // const login = () => {} // make a login request
+  // const register = () => {} // register the user
+  // const logout = () => {} // clear the token in localStorage and the user data
+  return (
+    <AuthContext.Provider
+      value={{
+        token,
+        setToken,
+        isLoggedIn: !!token,
+        logout: () => setToken(undefined),
+      }}
+      {...props}
+    />
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
