3e03777ed23cb2d480325592f58be2b79c05f3e2
Eric Quanz <eric.quanz@gmail.com>
2021-09-26 09:59:58 +0200

    Standardize "dirty" exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,3 @@
 // Had to create this file and top-level public folder to benefit
 // from create-react-app instead of setting up webpack & dependencies.
-import { main } from "./0.bin/client";
-
-main();
+import "./0.bin/client";
