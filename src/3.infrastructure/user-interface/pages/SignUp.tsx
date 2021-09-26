f9e9c37dae9e2217c0fe9d0fab94de4f650bfe0d
Eric Quanz <eric.quanz@gmail.com>
2021-09-26 22:24:40 +0200

    Sign up on front-end

--- src/3.infrastructure/user-interface/pages/SignUp.tsx
+++ src/3.infrastructure/user-interface/pages/SignUp.tsx
@@ -1,31 +1,68 @@
-import type { FunctionComponent } from "react";
+/* eslint-disable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment */
+import type { FormEvent, FunctionComponent } from "react";
 import { Link } from "react-router-dom";
 
-export const SignUp: FunctionComponent = () => (
-  <main>
-    <h1>Sign up</h1>
-    <form>
-      <fieldset>
-        <legend className="visually-hidden">Account details</legend>
+import { Role, useSignUpMutation } from "../graphql/generated-types-and-hooks";
 
-        <label htmlFor="email">Email</label>
-        <input id="email" type="email" required />
+export const SignUp: FunctionComponent = () => {
+  const [signUp, { data, error }] = useSignUpMutation();
 
-        <br />
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-        <label htmlFor="password">Password</label>
-        <input id="password" type="password" required />
+    const email = event.currentTarget.email.value;
+    const password = event.currentTarget.password.value;
+    const repeatPassword = event.currentTarget["repeat-password"].value;
+    const isOwner: boolean = event.currentTarget["is-owner"].checked;
 
-        <br />
+    if (password !== repeatPassword) {
+      return;
+    }
 
-        <label>
-          <input type="checkbox" name="is-owner" value="true" />
-          I'm a restaurant owner
-        </label>
-      </fieldset>
+    await signUp({
+      variables: {
+        input: {
+          email,
+          password,
+          role: isOwner ? Role.Owner : undefined,
+        },
+      },
+    });
+  };
 
-      <button>Sign up</button>
-    </form>
-    <Link to="/login">I already have an account</Link>
-  </main>
-);
+  return (
+    <main>
+      <h1>Sign up</h1>
+      {error && <p>{error.message}</p>}
+      {data && data.signUp.success && <p>Account created successfully!</p>}
+      <form onSubmit={onSubmit}>
+        <fieldset>
+          <legend className="visually-hidden">Account details</legend>
+
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" required />
+
+          <br />
+
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" required />
+
+          <br />
+
+          <label htmlFor="repeat-password">Repeat password</label>
+          <input id="repeat-password" type="password" required />
+
+          <br />
+
+          <label>
+            <input type="checkbox" name="is-owner" value="true" />
+            I'm a restaurant owner
+          </label>
+        </fieldset>
+
+        <button type="submit">Sign up</button>
+      </form>
+      <Link to="/login">I already have an account</Link>
+    </main>
+  );
+};
