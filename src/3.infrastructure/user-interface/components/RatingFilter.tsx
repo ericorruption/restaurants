68eb8fd0b8eacfb5bf14b2aa0b6e917dc32a8098
Eric Quanz <eric.quanz@gmail.com>
2021-09-27 17:53:37 +0200

    Improve main page design, add static rating filter

---
+++ src/3.infrastructure/user-interface/components/RatingFilter.tsx
@@ -0,0 +1,64 @@
+import type {
+  ChangeEvent,
+  ChangeEventHandler,
+  Dispatch,
+  FunctionComponent,
+  SetStateAction,
+} from "react";
+
+import type { NumberBetween1And5 } from "../../../1.domain/shared-kernel";
+
+import "./RatingFilter.css";
+
+export const RatingFilter: FunctionComponent<{
+  value?: NumberBetween1And5;
+  onChange: Dispatch<SetStateAction<NumberBetween1And5 | undefined>>;
+}> = ({ value, onChange }) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange(Number(event.target.value) as NumberBetween1And5);
+  };
+
+  const resetFilter = () => onChange(undefined);
+
+  return (
+    <>
+      <p>Filter by rating:</p>
+      <fieldset>
+        <legend className="visually-hidden">Rating</legend>
+
+        <RatingRadio value={5} checked={value === 5} onChange={handleChange} />
+        <RatingRadio value={4} checked={value === 4} onChange={handleChange} />
+        <RatingRadio value={3} checked={value === 3} onChange={handleChange} />
+        <RatingRadio value={2} checked={value === 2} onChange={handleChange} />
+        <RatingRadio value={1} checked={value === 1} onChange={handleChange} />
+
+        {value && (
+          <button
+            type="button"
+            className="rating-filter-button"
+            onClick={resetFilter}
+          >
+            Reset filter
+          </button>
+        )}
+      </fieldset>
+    </>
+  );
+};
+
+const RatingRadio: FunctionComponent<{
+  value: number;
+  checked: boolean;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+}> = ({ value, checked, onChange }) => (
+  <label>
+    <input
+      type="radio"
+      name="rating"
+      value={value}
+      onChange={onChange}
+      checked={checked}
+    />{" "}
+    {Array(value).fill("⭐").join("")}
+  </label>
+);
--- src/3.infrastructure/user-interface/pages/Main.tsx
+++ src/3.infrastructure/user-interface/pages/Main.tsx
@@ -1,25 +1,47 @@
-import type { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
+import { Link } from "react-router-dom";
 
+import type { NumberBetween1And5 } from "../../../1.domain/shared-kernel";
+import { RatingFilter } from "../components/RatingFilter";
 import { useListRestaurantsQuery } from "../graphql/generated-types-and-hooks";
 
-// TODO call /restaurants, render list
-// TODO filter by rating
+import "./Main.css";
+
+// TODO move filtering to back-end
+// TODO propagate rating to URL
 export const Main: FunctionComponent = () => {
   const { data, loading, error } = useListRestaurantsQuery();
+  const [rating, setRating] = useState<NumberBetween1And5>();
 
   return (
     <main>
-      <h1>Restaurants</h1>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error.message}</p>}
-      {data && data.restaurants.length && (
-        <ul>
-          {data.restaurants.map((restaurant) => (
-            <li key={restaurant.id}>{restaurant.name}</li>
-          ))}
-        </ul>
-      )}
-      {data && !data.restaurants.length && <p>No restaurants</p>}
+      <h1 className="main-page-title">Restaurants</h1>
+      <p>Sorted by average rating.</p>
+
+      <div className="main-page-grid">
+        <div>
+          <RatingFilter value={rating} onChange={setRating} />
+        </div>
+        <div>
+          {loading && <p>Loading...</p>}
+          {error && <p>Error: {error.message}</p>}
+          {data && data.restaurants.length && (
+            <ul>
+              {[...data.restaurants]
+                .filter((restaurant) => restaurant)
+                .map((restaurant) => (
+                  <li key={restaurant.id}>
+                    {/* TODO beautiful pic */}
+                    <Link to={`/restaurants/${restaurant.id}`}>
+                      {restaurant.name}
+                    </Link>
+                  </li>
+                ))}
+            </ul>
+          )}
+          {data && !data.restaurants.length && <p>No restaurants</p>}
+        </div>
+      </div>
     </main>
   );
 };
