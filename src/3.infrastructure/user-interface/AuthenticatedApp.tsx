a8b8f3d1bd9435408716653a004a262f5a20661d
Eric Quanz <eric.quanz@gmail.com>
2021-09-27 17:54:25 +0200

    Refactor: move logged in router to AuthenticatedApp component

--- src/3.infrastructure/user-interface/AuthenticatedApp.tsx
+++ src/3.infrastructure/user-interface/AuthenticatedApp.tsx
@@ -1,13 +1,31 @@
 import type { FunctionComponent } from "react";
+import {
+  Switch,
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
-import { Header } from "./Header";
-import { Router } from "./Router";
+import { Header } from "./components/Header";
+import { Main } from "./pages/Main";
+import { Restaurant } from "./pages/Restaurant";
 
 const AuthenticatedApp: FunctionComponent = () => (
-  <>
+  <Router>
     <Header />
-    <Router />
-  </>
+    <Switch>
+      <Route path="/" exact>
+        {/* TODO different for owner */}
+        <Main />
+      </Route>
+      <Route path="/restaurants/:restaurantId">
+        <Restaurant />
+      </Route>
+      <Route path="/login">
+        <Redirect to="/" />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 export default AuthenticatedApp;
--- src/3.infrastructure/user-interface/Router.tsx
+++
@@ -1,5 +0,0 @@
-import type { FunctionComponent } from "react";
-
-import { Main } from "./pages/Main";
-
-export const Router: FunctionComponent = () => <Main />;
